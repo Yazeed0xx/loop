b7d247b8c86e50d17f8d3387c4773321cd5bd81d
Yazeed0xx <yazeedthunyan@gmail.com>
2024-05-22 18:20:57 +0300

    lap 2

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
-for(let i=0; i<= 25; i+=2){
-    console.log(i)
+// for(let i=0; i<= 25; i+=2){
+//     console.log(i)
+// }
+
+// // for()
+
+
+
+//     // for(let odd=0; odd < 2 == 0; odd++){
+//     //     console.log(odd)
+//     // }
+
+
+// //    for(z =0; z<= 34; z)
+// //     console.log(z)
+// for (let b = 1; b <= 10; b++) {
+//     let xx = b * b;
+//     console.log(xx)
+// }
+
+function sum(num1, num2){
+    return(num1 + num2)
+
+
 }
+function min(num1, num2){
+    return(num1 - num2)
+    
 
-// for()
-
-
-
-    // for(let odd=0; odd < 2 == 0; odd++){
-    //     console.log(odd)
-    // }
-
-
-//    for(z =0; z<= 34; z)
-//     console.log(z)
-for (let b = 1; b <= 10; b++) {
-    let xx = b * b;
-    console.log(xx)
 }
+function div(num1, num2){
+    return(num1 / num2)
+    
+
+}
+var multi= function mult(num1, num2){
+    return(num1 * num2)
+    
+
+}
+console.log(sum(2, 2))
+console.log(min(1, 2))
+console.log(div(10, 2))
+console.log(multi(3, 2))
