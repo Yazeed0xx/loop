e3ca435e02d169a92bbaadffea00238eca4331a4
Yazeed0xx <yazeedthunyan@gmail.com>
2024-05-22 17:35:11 +0300

    lap

---
+++ main.js
@@ -0,0 +1,19 @@
+for(let i=0; i<= 25; i+=2){
+    console.log(i)
+}
+
+// for()
+
+
+
+    // for(let odd=0; odd < 2 == 0; odd++){
+    //     console.log(odd)
+    // }
+
+
+//    for(z =0; z<= 34; z)
+//     console.log(z)
+for (let b = 1; b <= 10; b++) {
+    let xx = b * b;
+    console.log(xx)
+}
