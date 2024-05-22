fc3105d90c024f49108b9694f550b769deebf868
Yazeed0xx <yazeedthunyan@gmail.com>
2024-05-22 18:50:16 +0300

    fuzz buzz

--- main.js
+++ main.js
@@ -42,3 +42,25 @@ console.log(sum(2, 2))
 console.log(min(1, 2))
 console.log(div(10, 2))
 console.log(multi(3, 2))
+
+
+
+
+function Fuzzbuzz(pp){
+if (pp %3 == 0 && pp %5 == 0)
+    return("FuzzBuzz")
+else if (pp % 5 == 0)
+    return("Buzz")
+else if(pp % 3 == 0)
+    return("fuzz")
+else{
+    return("not fuzzbuzz")
+
+
+}
+
+}
+console.log(Fuzzbuzz(15));
+console.log(Fuzzbuzz(5));
+console.log(Fuzzbuzz(3));
+console.log(Fuzzbuzz(4));
